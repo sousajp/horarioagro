aaf9db4835ee0d24d2dc24ee7a443cccc91d09e3
João Pedro R. de Sousa <95306281+sousajp@users.noreply.github.com>
2024-01-07 14:38:52 -0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,57 @@
+    document.getElementById("bta-1").addEventListener("click", function () 
+    {
+        var um = document.getElementById("10");
+        var dois = document.getElementById("15");
+        um.innerText = "Climatologia Agrícola";
+        dois.innerText = "Climatologia Agrícola"; 
+    });
+    document.getElementById("bta-2").addEventListener("click", function () 
+    {
+        var um = document.getElementById("6");
+        var dois = document.getElementById("8");
+        um.innerText = "Melhoramento de Plantas";
+        dois.innerText = "Melhoramento de Plantas"; 
+    });
+
+    document.getElementById("bta-3").addEventListener("click", function() {
+        var um = document.getElementById("19");
+        //var dois = document.getElementById("22");
+        um.innerText = "Melhoramento Animal";
+        //dois.innerText = "Melhoramento Animal";
+    });
+   
+
+    document.getElementById("bta-4").addEventListener("click", function() {
+        var um = document.getElementById("3");
+        var dois = document.getElementById("18");
+        um.innerText = "Princípios de Entomologia";
+        dois.innerText = "Princípios de Entomologia"; 
+    });
+
+    document.getElementById("bta-5").addEventListener("click", function() {
+        var um = document.getElementById("1");
+        var dois = document.getElementById("24");
+        um.innerText = "Horticultura Geral";
+        dois.innerText = "Horticultura Geral"; 
+    });
+
+    document.getElementById("bta-6").addEventListener("click", function() 
+    {
+        var um = document.getElementById("16");
+        var dois = document.getElementById("4");
+        um.innerText = "Forragicultura";
+        dois.innerText = "Forragicultura"; 
+    });
+    
+    document.getElementById("bta-7").addEventListener("click", function() {
+        var um = document.getElementById("7");
+        var dois = document.getElementById("17");
+        um.innerText = "Fitopatologia I";
+        dois.innerText = "Fitopatologia I";});
+
+    document.getElementById("bta-8").addEventListener("click", function() {
+        var um = document.getElementById("2");
+        var dois = document.getElementById("21");
+        um.innerText = "Fertilidade do Solo";
+        dois.innerText = "Fertilidade do Solo";
+    });
